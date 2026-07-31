871742aa2b72fbc0816c234088c545aef22b7288
ededward <eddyedwardwu7882@gmail.com>
2026-07-30 22:47:02 -0700

    Updated corner markers

--- src/App.tsx
+++ src/App.tsx
@@ -5,18 +5,26 @@ function App() {
   const rows = 10;
   const cols = 9;
 
-  const markPositions = [
+  const fullMarkPositions = [
     // top cannons
     20, 26,
 
     // top soldiers
-    28, 30, 32, 34, 36,
+    30, 32, 34,
 
     // bottom soldiers
-    55, 57, 59, 61, 63,
+    57, 59, 61,
 
     // bottom cannons
     65, 71,
+  ];
+
+  const leftMarkPositions = [
+    28, 55,
+  ];
+
+  const rightMarkPositions = [
+    36, 63,
   ];
 
   return (
@@ -24,11 +32,25 @@ function App() {
       <h1>象棋</h1>
 
       <div className="board">
-        {Array.from({ length: rows * cols }).map((_, index) => (
-          <div key={index} className="point">
-            {markPositions.includes(index + 1) && <CornerMark />}
-          </div>
-        ))}
+        {Array.from({ length: rows * cols }).map((_, index) => {
+          const position = index + 1;
+
+          return (
+            <div key={index} className="point">
+              {fullMarkPositions.includes(position) && (
+                <CornerMark />
+              )}
+
+              {leftMarkPositions.includes(position) && (
+                <CornerMark side="left" />
+              )}
+
+              {rightMarkPositions.includes(position) && (
+                <CornerMark side="right" />
+              )}
+            </div>
+          );
+        })}
 
         <div className="river">
           <span>楚河</span>
--- src/components/CornerMark.tsx
+++ src/components/CornerMark.tsx
@@ -1,6 +1,6 @@
-function CornerMark() {
+function CornerMark({ side = "full" }) {
   return (
-    <div className="corner-mark">
+    <div className={`corner-mark ${side}`}>
       <div className="corner top-left" />
       <div className="corner top-right" />
       <div className="corner bottom-left" />
