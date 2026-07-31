42c1630c23396b7b52df319882f0ce487c612719
ededward <eddyedwardwu7882@gmail.com>
2026-07-30 22:13:57 -0700

    Palace diagonal and corner mark v1

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,23 @@
 import "./App.css";
+import CornerMark from "./components/CornerMark";
 
 function App() {
   const rows = 10;
   const cols = 9;
+
+  const markPositions = [
+    // top cannons
+    20, 26,
+
+    // top soldiers
+    28, 30, 32, 34, 36,
+
+    // bottom soldiers
+    55, 57, 59, 61, 63,
+
+    // bottom cannons
+    65, 71,
+  ];
 
   return (
     <div className="app">
@@ -10,12 +25,24 @@ function App() {
 
       <div className="board">
         {Array.from({ length: rows * cols }).map((_, index) => (
-          <div key={index} className="point" />
+          <div key={index} className="point">
+            {markPositions.includes(index + 1) && <CornerMark />}
+          </div>
         ))}
 
         <div className="river">
           <span>楚河</span>
           <span>漢界</span>
+        </div>
+
+        <div className="palace top-palace">
+          <div className="diagonal diagonal-1"></div>
+          <div className="diagonal diagonal-2"></div>
+        </div>
+
+        <div className="palace bottom-palace">
+          <div className="diagonal diagonal-3"></div>
+          <div className="diagonal diagonal-4"></div>
         </div>
       </div>
     </div>
---
+++ src/components/CornerMark.tsx
@@ -0,0 +1,12 @@
+function CornerMark() {
+  return (
+    <div className="corner-mark">
+      <div className="corner top-left" />
+      <div className="corner top-right" />
+      <div className="corner bottom-left" />
+      <div className="corner bottom-right" />
+    </div>
+  );
+}
+
+export default CornerMark;
