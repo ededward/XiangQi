040cf251e104ce5c9c5d9027b15f063c4b8e98d2
ededward <eddyedwardwu7882@gmail.com>
2026-08-03 00:29:12 -0700

    Move logic for general

--- src/logic/getLegalMoves.ts
+++ src/logic/getLegalMoves.ts
@@ -2,6 +2,8 @@ import type { Piece } from "../types/Piece";
 import { getChariotMoves } from "./movements/chariot";
 import { getHorseMoves } from "./movements/horse";
 import { getElephantMoves } from "./movements/elephant";
+import { getAdvisorMoves } from "./movements/advisor";
+import { getGeneralMoves } from "./movements/general";
 
 export function getLegalMoves(piece: Piece, pieces: Piece[]): number[] {
   switch (piece.type) {
@@ -13,6 +15,12 @@ export function getLegalMoves(piece: Piece, pieces: Piece[]): number[] {
 
     case "elephant":
       return getElephantMoves(piece, pieces);
+
+    case "advisor":
+      return getAdvisorMoves(piece, pieces);
+
+    case "general":
+      return getGeneralMoves(piece, pieces);
 
     default:
       return [];
