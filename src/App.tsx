d2b7c76fddf1761dd5302d04fb05ba854c87443e
ededward <eddyedwardwu7882@gmail.com>
2026-08-04 22:48:12 -0700

    Added player and turn tracking

--- src/App.tsx
+++ src/App.tsx
@@ -2,32 +2,30 @@ import { useState } from "react";
 import "./App.css";
 import Board from "./components/Board";
 import { initialPieces } from "./data/initialPositions";
-import type { Piece } from "./types/Piece";
+import type { Side, Piece } from "./types/Piece";
 import { getLegalMoves } from "./logic/getLegalMoves";
 
 function App() {
   const [pieces, setPieces] = useState(initialPieces);
   const [selectedPiece, setSelectedPiece] = useState<Piece | null>(null);
+  const [currentPlayer, setCurrentPlayer] = useState<Side>("red");
+  const [gameMode, setGameMode] = useState<"single" | "double">("double");
   const legalMoves = selectedPiece ? getLegalMoves(selectedPiece, pieces) : [];
 
   function handlePointClick(position: number) {
     if (!selectedPiece) return;
-
     if (!legalMoves.includes(position)) return;
-
-    const remainingPieces = pieces.filter(piece => piece.position !== position);
-    const updatedPieces = remainingPieces.map(piece => {
-      if (piece.position === selectedPiece.position) {
-        return { ...piece, position };
-      }
-      return piece;
-    });
     
-    setPieces(updatedPieces);
+    setPieces(movePiece(pieces, selectedPiece, position));
     setSelectedPiece(null);
+    setCurrentPlayer(player =>
+      player === "red" ? "black" : "red"
+    );
   }
 
   function handlePieceClick(piece: Piece) {
+    if (piece.side !== currentPlayer) return;
+
     if (selectedPiece?.position === piece.position) {
       setSelectedPiece(null);
     } else {
@@ -35,9 +33,28 @@ function App() {
     }
   }
 
+  function movePiece(pieces: Piece[], selectedPiece: Piece, position: number) {
+    const remainingPieces = pieces.filter(piece => piece.position !== position);
+    const updatedPieces = remainingPieces.map(piece => {
+      if (piece.position === selectedPiece.position) {
+        return {...piece, position};
+      }
+      return piece;
+    });
+    return updatedPieces;
+  }
+
   return (
     <div className="app">
       <h1>象棋</h1>
+
+      <h2>
+        {
+          currentPlayer === "red"
+            ? "🔴 Red's Turn"
+            : "⚫ Black's Turn"
+        }
+      </h2>
 
       <Board 
         pieces={pieces}
